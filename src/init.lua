4f0da5a9f22777f692aacd56e8b131700649418f
TheNexusAvenger <zpcook@googlemail.com>
2020-09-27 18:58:33 -0400

    Fixed divide by 0 case if the size constraint is set before the size.

--- src/init.lua
+++ src/init.lua
@@ -212,9 +212,13 @@ function NexusButton:__new()
 		end
 	end))
 	table.insert(self.__Events,BorderAdorn:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
-		local BorderSizeRelative = (BorderAdorn.AbsoluteSize.Y/AdornFrame.AbsoluteSize.Y) - 1
-		local BorderCornerCutRelative = (CORNER_CUT_BACKGROUND_RELATIVE/math.sqrt(2)) / (1 + BorderSizeRelative)
-		self.BorderCutFrame:CutCorner("Bottom","Right",UDim2.new(BorderCornerCutRelative,0,BorderCornerCutRelative,0),"RelativeYY")
+		if AdornFrame.AbsoluteSize.Y == 0 then
+			self.BorderCutFrame:CutCorner("Bottom","Right",UDim2.new(0,0,0,0),"RelativeYY")
+		else
+			local BorderSizeRelative = (BorderAdorn.AbsoluteSize.Y/AdornFrame.AbsoluteSize.Y) - 1
+			local BorderCornerCutRelative = (CORNER_CUT_BACKGROUND_RELATIVE/math.sqrt(2)) / (1 + BorderSizeRelative)
+			self.BorderCutFrame:CutCorner("Bottom","Right",UDim2.new(BorderCornerCutRelative,0,BorderCornerCutRelative,0),"RelativeYY")
+		end
 	end))
 	table.insert(self.__Events,GamepadIcon:GetPropertyChangedSignal("IconVisible"):Connect(function()
 		self:__UpdateColors()
